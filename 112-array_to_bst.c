bf1b26bb3aa812b468df0eb02c94ee2088ff7ae9
T-redd01 <123007873+T-redd01@users.noreply.github.com>
2023-11-03 05:54:09 +0200

    Update 112-array_to_bst.c
    Edit on github

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -12,6 +12,9 @@ bst_t *array_to_bst(int *array, size_t size)
 	size_t i;
 	bst_t *root = NULL;
 
+	if (!array)
+		return (NULL);
+
 	root = bst_insert(&root, array[0]);
 	for (i = 1; i < size; i++)
 		bst_insert(&root, array[i]);
